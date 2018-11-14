a6cb7159d99fbe11691b37798af6538a078e4c7a
mac <mac@Jade-MBP.local>
2018-11-14 17:44:25 +0700

    re-arrange menu. User login to see advanced menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LocalService } from './local.service';
 
 @Component({
   selector: 'app-root',
@@ -11,25 +12,37 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
-    { title: 'Profile', url: '/profile', icon: 'home' },
-    { title: 'My Trip', url: '/my-trip', icon: 'home' },
+    // { title: 'Profile', url: '/profile', icon: 'home' },
+    // { title: 'My Trip', url: '/my-trip', icon: 'home' },
     { title: 'Locations', url: '/locations', icon: 'home' },
     // { title: 'Reservation', url: '/reservation', icon: 'home' },
-    { title: 'Travel journal', url: '/travel-journal', icon: 'home' },
+    // { title: 'Travel journal', url: '/travel-journal', icon: 'home' },
     { title: 'News/Special', url: '/news-special', icon: 'home' },
     { title: 'Language', url: '/language', icon: 'home' },
     { title: 'Booking Policy', url: '/booking-policy', icon: 'home' },
     // { title: 'List', url: '/list', icon: 'list' },
     // { title: 'Hot Deals', url: '/deals', icon: 'list' },
-    { title: 'Login/Register', url: '/account', icon: 'list' },
+    // { title: 'Login/Register', url: '/account', icon: 'list' },
     // { title: 'For Test', url: '/test', icon: 'list' },
     // { title: 'RxJS', url: '/rxjs', icon: 'list' }
   ];
 
+  public appPages1 = [
+    { title: 'Login/Register', url: '/account', icon: 'list' },
+  ];
+
+  public appPages2 = [
+    { title: 'Profile', url: '/profile', icon: 'home' },
+    { title: 'My Trip', url: '/my-trip', icon: 'home' },
+    { title: 'Travel journal', url: '/travel-journal', icon: 'home' },
+    { title: 'Logout', url: '/account', icon: 'list' },
+  ];
+  isSigned: boolean = false;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private localService: LocalService
   ) {
     this.initializeApp();
   }
@@ -41,6 +54,9 @@ export class AppComponent {
     });
   }
 
-  
+  menuOpen(){
+    console.log('menuOpen');
+    this.isSigned = this.localService.ACCOUNT.isSigned
+  }
 
 }
